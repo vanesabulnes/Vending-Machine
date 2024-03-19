fe172aded86483c4d579f68c58e55e2b2e66edca
Vanesa Bulnes <114104256+vanesabulnes@users.noreply.github.com>
2024-03-19 11:44:54 -0700

    VendingMachine.java

--- VendingMachine.java
+++ VendingMachine.java
@@ -2,6 +2,8 @@ import  java.util.Scanner;
 
 public class VendingMachine {
     public static void main(String[] args) {
+		 System.out.println("Please select which drink you want to order");
+            System.out.println("0 for Tea,\n1 for Espresso, \n2 for Americano, \n3 for Water");
         String[] menu = {"Tea", "Espresso", "Americano", "Water", "Hot Chocolate"};
         Scanner sc = new Scanner(System.in);
         //your code goes here
